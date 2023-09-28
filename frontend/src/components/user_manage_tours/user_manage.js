ca2e46275c71c23430124495013cde3c5d4ed7f2
Karicharla Harinadh <karicharlaharinadh@jmangroup.com>
2023-09-28 12:05:44 +0530

    12:05

--- frontend/src/components/user_manage_tours/user_manage.js
+++ frontend/src/components/user_manage_tours/user_manage.js
@@ -2,51 +2,60 @@ import React, { useState } from 'react';
 import { toast } from 'react-toastify';
 import { getToursUser } from '../../services/user_manage/user_manage_services';
 import { createnewBookingTour } from '../../services/user_manage/user_manage_services';
-import "../../styles/buttons.css"
+import jsPDF from 'jspdf'; 
+import "../../styles/buttons.css";
 
 const UserManageTours = () => {
     const [accordionOpen, setAccordionOpen] = useState(true);
     var [tableData, setTableData] = useState([]);
 
-
     const toggleAccordion = () => {
         setAccordionOpen(!accordionOpen);
     };
 
-
-    // Booking function frontend service
     const handleBook = async (index) => {
-
         let email_from_session = sessionStorage.getItem("email");
         console.log("email", email_from_session);
         console.log("handle book", tableData[index]);
         const data_dic = tableData[index];
-        data_dic["email"] = email_from_session
-        console.log(data_dic)
-        // calling the service
-        let newBooking = await createnewBookingTour(data_dic)
-            .then((newBooking => {
-                toast.success('Tour booked successfull !', {
-                    position: toast.POSITION.TOP_RIGHT
-                });
-            }))
+        data_dic["email"] = email_from_session;
+        console.log(data_dic);
+
+        let newBooking = await createnewBookingTour(data_dic);
+
+        if (newBooking) {
+            toast.success('Tour booked successfully!', {
+                position: toast.POSITION.TOP_RIGHT
+            });
+
+            // Generating the booking details PDF
+            const doc = new jsPDF();
+            doc.text("Booking Details", 10, 10); 
+
+            const data = tableData[index];
+            let yPosition = 30;
+
+            for (const key in data) {
+                if (data.hasOwnProperty(key)) {
+                    const value = data[key];
+                    doc.text(`${key}: ${value}`, 10, yPosition);
+                    yPosition += 10; 
+                }
+            }
+
+            // Saveing the booking details PDF
+            doc.save('booking_details.pdf');
+        }
     }
-
-
-    // Load tours for user frontend service
 
     const getTourdetails = async () => {
         console.log("tours button is clicked");
         let tours = await getToursUser();
-        let toursarray = Object.values(tours.get_tours)
-        tableData = toursarray
-        setTableData(toursarray)
-        console.log("table frontend", tableData)
+        let toursarray = Object.values(tours.get_tours);
+        tableData = toursarray;
+        setTableData(toursarray);
+        console.log("table frontend", tableData);
     }
-    //   useEffect(() => {
-    //     getTourdetails();
-    //   }, []);
-
 
     return (
         <div>
@@ -60,7 +69,6 @@ const UserManageTours = () => {
                         <table className="responsive-table">
                             <thead>
                                 <tr>
-                                    {/* <th>S.NO</th> */}
                                     <th>Tour Name</th>
                                     <th>Tour Description</th>
                                     <th>Tour Offer Price</th>
@@ -71,7 +79,6 @@ const UserManageTours = () => {
                             <tbody>
                                 {tableData.map((record, index) => (
                                     <tr key={index}>
-                                        {/* <td>{index}</td> */}
                                         <td>{record.tour_name}</td>
                                         <td>{record.description}</td>
                                         <td>{record.tour_original_price}</td>
@@ -94,5 +101,110 @@ const UserManageTours = () => {
 };
 
 export default UserManageTours;
+
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react';
+// import { toast } from 'react-toastify';
+// import { getToursUser } from '../../services/user_manage/user_manage_services';
+// import { createnewBookingTour } from '../../services/user_manage/user_manage_services';
+// import "../../styles/buttons.css"
+
+// const UserManageTours = () => {
+//     const [accordionOpen, setAccordionOpen] = useState(true);
+//     var [tableData, setTableData] = useState([]);
+
+
+//     const toggleAccordion = () => {
+//         setAccordionOpen(!accordionOpen);
+//     };
+
+
+//     // Booking function frontend service
+//     const handleBook = async (index) => {
+
+//         let email_from_session = sessionStorage.getItem("email");
+//         console.log("email", email_from_session);
+//         console.log("handle book", tableData[index]);
+//         const data_dic = tableData[index];
+//         data_dic["email"] = email_from_session
+//         console.log(data_dic)
+//         // calling the service
+//         let newBooking = await createnewBookingTour(data_dic)
+//             .then((newBooking => {
+//                 toast.success('Tour booked successfull !', {
+//                     position: toast.POSITION.TOP_RIGHT
+//                 });
+//             }))
+//     }
+
+
+//     // Load tours for user frontend service
+
+//     const getTourdetails = async () => {
+//         console.log("tours button is clicked");
+//         let tours = await getToursUser();
+//         let toursarray = Object.values(tours.get_tours)
+//         tableData = toursarray
+//         setTableData(toursarray)
+//         console.log("table frontend", tableData)
+//     }
+//     //   useEffect(() => {
+//     //     getTourdetails();
+//     //   }, []);
+
+
+//     return (
+//         <div>
+//             <div className='my-heading'><h2> Our <span>packages</span> </h2></div>
+//             <div className="accordion">
+//                 <div className={`accordion-header ${accordionOpen ? 'open' : ''}`} onClick={toggleAccordion}>
+//                     <p> Our Trending Packages</p>
+//                 </div>
+//                 {accordionOpen && (
+//                     <div className="accordion-content">
+//                         <table className="responsive-table">
+//                             <thead>
+//                                 <tr>
+//                                     {/* <th>S.NO</th> */}
+//                                     <th>Tour Name</th>
+//                                     <th>Tour Description</th>
+//                                     <th>Tour Offer Price</th>
+//                                     <th>Tour Original Price</th>
+//                                     <th>Actions</th>
+//                                 </tr>
+//                             </thead>
+//                             <tbody>
+//                                 {tableData.map((record, index) => (
+//                                     <tr key={index}>
+//                                         {/* <td>{index}</td> */}
+//                                         <td>{record.tour_name}</td>
+//                                         <td>{record.description}</td>
+//                                         <td>{record.tour_original_price}</td>
+//                                         <td>{record.tour_discount_price}</td>
+//                                         <td>
+//                                             <button className="my-btn" onClick={() => handleBook(index)}>Book</button>
+//                                         </td>
+//                                     </tr>
+//                                 ))}
+//                             </tbody>
+//                         </table>
+//                     </div>
+//                 )}
+//             </div>
+//             <br /><button onClick={getTourdetails} className="my-btn">
+//                 view tours
+//             </button>
+//         </div>
+//     );
+// };
+
+// export default UserManageTours;
 
 
